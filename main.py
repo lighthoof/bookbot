d070bd73d599da6afeca3bf948c0537bfe3c142a
lighthoof <andrew.kuligin@gmail.com>
2025-01-16 12:16:40 +0200

    switch printing book to counting words and printing number

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def readBook(filepath):
         file_contents = f.read()
     return file_contents
 
+def countWords(text):
+    words = text.split()
+    return len(words)
+
 def main():
-    print(readBook("books/frankenstein.txt"))
+    book_path = "books/frankenstein.txt"
+    book_text = readBook(book_path)
+    word_count = countWords(book_text)
+    print(f"Counted {word_count} words in the book")
 
 main()
