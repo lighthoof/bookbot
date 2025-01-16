78c1f8a6f6b1e4744d3ec4b6a48b1f77488995a6
lighthoof <andrew.kuligin@gmail.com>
2025-01-16 14:16:50 +0200

    added a function to sort by count of characters desc

--- main.py
+++ main.py
@@ -18,23 +18,51 @@ def countCharacters(text):
     lowered_text = text.lower()
     #looping over the string one char at a time
     for char in lowered_text:
-        if char in char_counts:
+        if char in char_counts and char.isalpha():
             char_counts[char] += 1
-        elif char not in char_counts:
+        elif char not in char_counts and char.isalpha():
             char_counts[char] = 1
 
     return char_counts
 
-#formatting char counts into a readable report
-def formatCharCounts(unformatted_char_counts):
-    char_report = "Character counts :"
+#sorting char counts alphabetically
+def sortedCharCountsAlph(unformatted_char_counts):
+    char_counts = {}
     #sort counted characters alphabetically
     sorted_char_list = sorted(unformatted_char_counts)
     #loop through sorte char sequence and append the counts to a report string
     for char in sorted_char_list:
-        char_report += f"{char} : {unformatted_char_counts[char]}\n"
+        char_counts[char] = unformatted_char_counts[char]
 
-    return char_report    
+    return char_counts    
+
+def sortedCharCountsNum(unformatted_char_counts): 
+    #function to return value of "num" to use in sort
+    def sort_on(dict):
+        return dict["num"]
+    
+    char_counts = {}
+    list_of_char_counts = []
+
+    #convert input dictionary into a list of dictionaries for sorting
+    for char in unformatted_char_counts:
+        char_count_dict = {}
+        char_count_dict["name"] = char
+        char_count_dict["num"] = unformatted_char_counts[char]
+        list_of_char_counts.append(char_count_dict)
+    
+    #sort the list of dictionaries
+    list_of_char_counts.sort(reverse=True, key=sort_on)
+    
+    #convert list of dictionaries back to a dictionary
+    for dict in list_of_char_counts:
+        char_counts[dict["name"]] = dict["num"]
+
+    return char_counts
+
+def formatReport(word_count,char_count):
+
+    return
 
 def main():
     book_path = "books/frankenstein.txt"
@@ -42,7 +70,8 @@ def main():
     word_count = countWords(book_text)
     counted_characters = countCharacters(book_text)
 
-
-    print(f"Counted {word_count} words in the book\n" + formatCharCounts(counted_characters))
+    #print(sortedCharCountsAlph(counted_characters))
+    sortedCharCountsNum(counted_characters)
+    #print(f"Counted {word_count} words in the book\n" + formatCharCounts(counted_characters))
 
 main()
